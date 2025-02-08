acf66d0ec0e9372443bf26d98b15fe90acd2efa7
qchris101 <qchris101@yahoo.com>
2025-02-07 23:05:48 -0500

    Express server is fetching Weather Api data. Frontend is receiving express server data/calls

--- public/app.js
+++ public/app.js
@@ -1,22 +1,31 @@
 
+const form = document.querySelector("#getWeather")
 
+const input = document.querySelector("#location")
 
-
-const form = document.querySelector("#location")
-
-const input = document.querySelector("#input")
-
-// input.addEventListener('input', () => {
-//     console.log(input.value)
-// })
-
-form.addEventListener("submit", function(e){
+form.addEventListener("click", async (e) =>{
     e.preventDefault()    
     const location = input.value
         try{
-            const response =  fetch(`/weather?city=${location}`)
-            const data = response.json()
-            console.log(data.current.temp_f)
+            const response = await fetch(`http://localhost:8000/weather?city=${location}`)
+            const data = await response.json()
+            // console.log(data.location.name)
+            // console.log(data.current.temp_f)
+            // console.log(data.location.localtime)
+            // console.log(data.current.condition.text)
+            // const locationData = {
+            //     "name": data.location.name,
+            //     "localTime": data.location.localtime,
+            //     "localTemp": data.current.temp_f
+            // }
+            console.log(data)
+            // Object.entries(data.location).forEach((key,value) => {
+            //     console.log(`${key}: ${value}`)
+            // })
+
+            const img = document.createElement('img')
+            img.setAttribute('src', `https:${data.current.condition.icon}`)
+            document.body.appendChild(img)
         } catch (error) {
             console.error("Error fetching data:", error)
         }
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 require("dotenv").config()
 const express = require("express")
+const cors =require("cors")
 const app = express()
 const PORT = 8000
-
+app.use(cors())
+// console.log("API Key:", apiKey)
 app.get('/weather', async (req,res) => {
     const city = req.query.city 
+    // console.log(req.query.city)
     const apiKey = process.env.WEATHER_API_KEY
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
 
     try {
         const response = await fetch(url)
         const data = await response.json()
+        console.log(data)
         res.json(data)
     }catch (error){
         res.status(500).json({ error: "Failed to fetch data."})
